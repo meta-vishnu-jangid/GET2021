75d3945c919e434a26ed9ac230d3b80b1e961195
Vishnu Jangid <vishnu.jangid@metacube.com>
2021-03-18 22:49:23 +0530

    added Update and Delete Cascade

--- DBMS/Commands And Joins/assignment2_ques_1.sql
+++ DBMS/Commands And Joins/assignment2_ques_1.sql
@@ -6,68 +6,82 @@ create database storeFront;
 
 use storeFront;
 
-#Creating user table 
+#creating users table
 create table users (  
     id INT NOT NULL AUTO_INCREMENT, 
     first_name VARCHAR(50) NOT NULL, 
     last_name VARCHAR(50) NOT NULL,
-    type VARCHAR(7) CHECK(type IN ("admin", "shopper")), 
+    email VARCHAR(50) NOT NULL,
+    type VARCHAR(7) CHECK(type IN ("admin","shopper") ), 
     PRIMARY KEY(id)
 ) ;
 
-#Creating address table
+#creating zipcode table
+CREATE TABLE zipcode(
+    city VARCHAR(30) NOT NULL,
+    state VARCHAR(30) NOT NULL,
+    pincode VARCHAR(6) NOT NULL,
+    PRIMARY KEY(pincode)
+);
+
+
+#creating address table
 CREATE TABLE address
 (
     id INTEGER AUTO_INCREMENT,
     user_id INTEGER NOT NULL,
     house_no VARCHAR(7) NOT NULL,
     street VARCHAR(30) NOT NULL,
-    city VARCHAR(30) NOT NULL,
-    state VARCHAR(30) NOT NULL,
     country VARCHAR(30) DEFAULT "India",
     pincode VARCHAR(6) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(pincode) REFERENCES zipcode(pincode) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-#Creating category table
+
+#creating category table
 create table category (  
     id INT NOT NULL AUTO_INCREMENT , 
     title VARCHAR(100) NOT NULL , 
-    parent_category INT, PRIMARY KEY(ID), 
-    FOREIGN KEY(parent_category) REFERENCES category(id) 
+    parent_category INT, 
+    PRIMARY KEY(ID), 
+    FOREIGN KEY(parent_category) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
-#Creating product table
+
+#creating product table
 create table product (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    price VARCHAR(10) NOT NULL,
+    price VARCHAR(10) NOT NULL CHECK ( price > 0),
     description TEXT NOT NULL,
-    quantity INT(5) NOT NULL,
+    status VARCHAR(8) DEFAULT "ACTIVE" CHECK (status IN ("active","inactive")),
+    quantity INT(5) NOT NULL CHECK ( quantity >= 0),
     PRIMARY KEY(id)
 ) ;
 
-#Creating product_category table
+
+#creating table for map between category and product
 CREATE TABLE product_category(
     product_id INT NOT NULL,
     category_id INT NOT NULL,
     PRIMARY KEY(product_id, category_id),
-    FOREIGN KEY(product_id) REFERENCES product(id),
-    FOREIGN KEY(category_id) REFERENCES category(id) 
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE 
 );
 
-#Creating product_image table
+
+#creataing product_image table
 create table product_image (
     id INTEGER AUTO_INCREMENT,
     product_id INT NOT NULL, 
     image_url TEXT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(product_id) REFERENCES product(id)
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
-#Creating orders table
+#cretaing category table
 CREATE TABLE orders (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
@@ -75,42 +89,42 @@ CREATE TABLE orders (
     total FLOAT NOT NULL,
     address_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(address_id) REFERENCES address(id)
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(address_id) REFERENCES address(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
-#Creating order_item table to store items of orders
+#creating order_item table
 CREATE TABLE order_item (
     order_id INT NOT NULL,
     product_id INT NOT NULL,
     quantity INT NOT NULL,
     deliver_time DATETIME,
     status VARCHAR(30) CHECK(status IN ("shipped", "placed","delivered","returned")) ,
-    PRIMARY KEY(order_id,product_id),
-    FOREIGN KEY(product_id) REFERENCES product(id)
+    PRIMARY KEY(order_id,product_id) ,
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 
 SHOW TABLES;
 
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS order_item;
-DROP TABLE IF EXISTS  product_image;
-DROP TABLE IF EXISTS  product_category;
-DROP TABLE IF EXISTS  product;
+DROP TABLE orders;
+DROP TABLE order_item;
+DROP TABLE product_image;
+DROP TABLE product_category;
+DROP TABLE product;
 
 
 SHOW TABLES;
 
-# again creating all tables
+#again creating table
 create table product (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
-    price VARCHAR(10) NOT NULL,
+    price VARCHAR(10) NOT NULL CHECK ( price > 0),
     description TEXT NOT NULL,
-    quantity INT(5) NOT NULL,
+    status VARCHAR(8) DEFAULT "ACTIVE" CHECK (status IN ("active","inactive")),
+    quantity INT(5) NOT NULL CHECK ( quantity >= 0),
     PRIMARY KEY(id)
 ) ;
 
@@ -118,8 +132,8 @@ CREATE TABLE product_category(
     product_id INT NOT NULL,
     category_id INT NOT NULL,
     PRIMARY KEY(product_id, category_id),
-    FOREIGN KEY(product_id) REFERENCES product(id),
-    FOREIGN KEY(category_id) REFERENCES category(id) 
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE ON UPDATE CASCADE  
 );
 
 
@@ -129,7 +143,7 @@ create table product_image (
     product_id INT NOT NULL, 
     image_url TEXT NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(product_id) REFERENCES product(id)
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE orders (
@@ -139,8 +153,8 @@ CREATE TABLE orders (
     total FLOAT NOT NULL,
     address_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(address_id) REFERENCES address(id)
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    FOREIGN KEY(address_id) REFERENCES address(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE order_item (
@@ -148,9 +162,7 @@ CREATE TABLE order_item (
     product_id INT NOT NULL,
     quantity INT NOT NULL,
     deliver_time DATETIME,
-    status VARCHAR(30) CHECK(status IN ("shipped", "placed","delivered","returned")) ,
+    status VARCHAR(30) NOT NULL CHECK(status IN ("shipped", "placed","delivered","returned")) ,
     PRIMARY KEY(order_id,product_id),
-    FOREIGN KEY(product_id) REFERENCES product(id)
+    FOREIGN KEY(product_id) REFERENCES product(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-SHOW TABLES;
